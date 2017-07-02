810b434aa9caacb866651bb4611fdaf200ecd8fc
lazygeniuslanz <lanz.irlanda@gmail.com>
2017-07-02 12:07:07 +0800

    header and font

--- js/functions.js
+++ js/functions.js
@@ -48,7 +48,7 @@ function navshow(){
 	var div_top = $('#nav-line').offset().top;
 	/*
 	*/
-	if(window_top > (div_top - 200) && loaded){ 
+	if(window_top > (div_top + 200) && loaded){ 
 		$('#navbar').addClass('shownav');
 		$('#navbar').removeClass('hidenav');
 		
